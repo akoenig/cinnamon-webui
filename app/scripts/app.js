4b8228e46efcd3968e4214d35167bc8ea83ef48e
André König <andre.koenig@gmail.com>
2013-10-05 01:49:11 +0200

    Initital Revision. Project structure.

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+/*
+ * cinnamon-webui
+ *
+ * A continuous integration server on top of substacks cicada.
+ *
+ * Copyright(c) 2013 André König <akoenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+'use strict';
+
+var akoenig = akoenig || {};
+
+akoenig.cinnamon = angular.module('akoenig.cinnamon', ['ngRoute']).config([
+    function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+              redirectTo: '/'
+            });
+    }
+]);
